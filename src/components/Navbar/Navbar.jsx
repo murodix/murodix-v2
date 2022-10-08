5f220ecc72d83bc2642259ce2921f14833487b68
Luis Miguel Muñoz Morales <lmumo95@gmail.com>
2022-10-08 18:08:38 -0400

    updating the logo container, and adding required to links prop

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,12 +16,15 @@ const Navbar = ({ links }) => {
     return (
         <div className={`${styles.navbar} ${isExpanded ? styles['navbar-expand'] : ""}`}>
             <div className={styles.navbar__container}>
-                <a href="#home" className={`${styles.navbar__logo} ${selectedLink === "#home" ? styles['navbar__logo-active'] : ""}`}
-                    onClick={() => selectLink("#home")}>
-                    <svg width="170" height="144" viewBox="0 0 170 144" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M163.319 72V12.6099L84.8795 72L45.6597 101.695M163.319 72V131.39L124.099 101.695M163.319 72L124.099 101.695M45.6597 101.695L6.43982 131.39V12.6099L45.6597 42.305L55.4646 49.7287M45.6597 101.695L84.8795 131.39L124.099 101.695" stroke="#FFFFFF" strokeWidth="12.2772" />
-                    </svg>
-                </a>
+                <div className={styles.navbar__logo__container}>
+                    <a href="#home" className={`${styles.navbar__logo} ${selectedLink === "#home" ? styles['navbar__logo-active'] : ""}`}
+                        onClick={() => selectLink("#home")}>
+                        <svg width="170" height="144" viewBox="0 0 170 144" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M163.319 72V12.6099L84.8795 72L45.6597 101.695M163.319 72V131.39L124.099 101.695M163.319 72L124.099 101.695M45.6597 101.695L6.43982 131.39V12.6099L45.6597 42.305L55.4646 49.7287M45.6597 101.695L84.8795 131.39L124.099 101.695" stroke="#FFFFFF" strokeWidth="12.2772" />
+                        </svg>
+                    </a>
+                </div>
+
                 <div className={`${styles.navbar__links} ${isExpanded ? styles['navbar__links-expand'] : ""}`}>
                     {links?.map((link, index) => {
                         return (
@@ -46,7 +49,7 @@ const Navbar = ({ links }) => {
 }
 
 Navbar.propTypes = {
-    links: PropTypes.array
+    links: PropTypes.array.isRequired
 }
 
 Navbar.defaultProps = {
