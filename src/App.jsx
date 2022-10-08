a34ae4633c512e88a557dcb4e5b6c3cb377055b3
Luis Miguel Muñoz Morales <lmumo95@gmail.com>
2022-10-08 15:29:17 -0400

    Adding stoke animation for svg logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import styles from './App.module.scss';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   console.log(styles)
   return (
     <div className={styles.app}>
-
+      <Navbar />
     </div>
   );
 }
