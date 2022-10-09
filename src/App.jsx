afb3e0e0cd0618fed64cbabbd5ae17c113bb5852
Luis Miguel Muñoz Morales <lmumo95@gmail.com>
2022-10-08 21:36:17 -0400

    Exported config to json, and retrieving it with fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
+import { useEffect, useState } from 'react';
 import styles from './App.module.scss';
 import Navbar from './components/Navbar/Navbar';
+import About from './pages/about/About';
+import Hero from './pages/hero/Hero';
 
 function App() {
-  const links = [
-    {
-      ref: "#about",
-      desc: "About"
-    },
-    {
-      ref: "#experience",
-      desc: "Experience"
-    },
-    {
-      ref: "#work",
-      desc: "Work"
-    }
-  ]
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, [])
+
   return (
     <div className={styles.app}  >
-      <Navbar links={links} />
+      <Navbar links={data?.links} />
+      <Hero />
+      <About />
     </div>
   );
 }
