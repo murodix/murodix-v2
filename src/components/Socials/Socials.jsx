1c8f1d87f0a35a8c82ec2bccbbaa46426dfd3222
Luis Miguel Muñoz Morales <lmumo95@gmail.com>
2022-10-09 19:49:31 -0400

    Adding animation to social when landing

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Socials.module.scss';
 import Social from '../Social/Social';
+import { gsap } from 'gsap/all';
 
 const Socials = ({ socials }) => {
+
+    const socialsRef = useRef();
+    const timeline = useRef();
+
+    useEffect(() => {
+        let context = gsap.context(() => {
+            console.log(styles.socials__container);
+            timeline.current = gsap.timeline()
+                .from("." + styles.socials__container, {
+                    y: 20,
+                    opacity: 0,
+                    duration: 1,
+                    scale: 0.9
+                }, "+=1")
+        }, socialsRef)
+
+        return () => context.revert();
+
+    }, [])
+
+
     return (
-        <div className={styles.socials}>
+        <div ref={socialsRef} className={styles.socials}>
             <div className={styles.socials__container}>
                 {socials?.map((social, index) => {
                     return (<Social key={index} name={social?.name} url={social?.url} />)
