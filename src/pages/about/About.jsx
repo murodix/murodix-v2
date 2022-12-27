1cc37ddfe5ca144c00ec26026db09f5aabe10371
Luis Miguel Muñoz Morales <lmumo95@gmail.com>
2022-12-27 13:50:19 -0500

    Adding an about section

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,7 +4,24 @@ import styles from './About.module.scss';
 const About = () => {
     return (
         <section id="about" className={styles.about}>
+            <div className={styles.about__wrapper}>
+                <div className={styles.about__title}>
+                    <h2>About me</h2>
+                </div>
+                <div className={styles.about__container}>
+                    <div className={styles.about__bio}>
+                        <p>
+                            I'm just a simple tech lover in a complicated Word. Making creative solutions for complicated people.
+                            I'm just a simple tech lover in a complicated Word. Making creative solutions for complicated people.
+                            I'm just a simple tech lover in a complicated Word. Making creative solutions for complicated people.
+                            I'm just a simple tech lover in a complicated Word. Making creative solutions for complicated people.
+                        </p>
+                    </div>
+                    <div className={styles.about__picture}>
 
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
